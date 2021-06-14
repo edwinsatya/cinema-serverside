f1577339978c6908f136603e666af1d85c4e3586
edwinsatya <edwinsatyayudistira@gmail.com>
2021-06-14 22:41:11 +0700

    update crud

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -6,7 +6,6 @@ class CommentController {
     try {
       const userId = req.decoded.id;
       const discusId = req.params.discusId;
-      console.log(discusId, "discusId");
       const { comment } = req.body;
       const responseComment = await Comment.create({
         userId,
@@ -37,6 +36,18 @@ class CommentController {
 
   static async find(req, res, next) {
     try {
+      let response;
+      if (req.query.commentId) {
+        let commentId = req.query.commentId;
+        response = await Comment.findById(commentId).populate("userId");
+      } else {
+        response = await Comment.find().populate("userId");
+      }
+      res.status(200).json({
+        data: response,
+        message: "Successful get all comment",
+        status: 200,
+      });
     } catch (error) {
       next(error);
     }
@@ -44,6 +55,20 @@ class CommentController {
 
   static async update(req, res, next) {
     try {
+      const { comment } = req.body;
+      const id = req.params.commentId;
+      const update = {
+        comment,
+      };
+      const option = {
+        new: true,
+      };
+      const response = await Comment.findByIdAndUpdate(id, update, option);
+      res.status(200).json({
+        data: response,
+        message: "Comment has been updated",
+        status: 200,
+      });
     } catch (error) {
       next(error);
     }
@@ -51,6 +76,13 @@ class CommentController {
 
   static async delete(req, res, next) {
     try {
+      const id = req.params.commentId;
+      const response = await Comment.findByIdAndDelete(id);
+      res.status(202).json({
+        data: response,
+        message: "Discussion has been deleted",
+        status: 202,
+      });
     } catch (error) {
       next(error);
     }
