b1b9a3a3fd6c80aeba29644abf2562406a0094e3
edwinsatya <edwinsatyayudistira@gmail.com>
2021-07-29 11:45:52 +0700

    [ADD] detail person and credit person

--- server/controllers/personController.js
+++ server/controllers/personController.js
@@ -14,6 +14,54 @@ class PersonController {
       next(error);
     }
   }
+
+  static async detail(req, res, next) {
+    try {
+      const { personId } = req.params;
+      const response = await Axios.get(
+        `https://api.themoviedb.org/3/person/${personId}?api_key=${process.env.TMDB_KEY}`
+      );
+
+      res.status(200).json({
+        data: response.data,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async detailCredits(req, res, next) {
+    try {
+      const { personId } = req.params;
+      const response = await Axios.get(
+        `https://api.themoviedb.org/3/person/${personId}/combined_credits?api_key=${process.env.TMDB_KEY}`
+      );
+
+      response.data.cast.sort((a, b) => {
+        if (a.release_date === "") {
+          return -1;
+        } else if (
+          new Date(a.release_date || a.first_air_date) >
+          new Date(b.release_date || b.first_air_date)
+        ) {
+          return -1;
+        } else if (
+          new Date(a.release_date || a.first_air_date) <
+          new Date(b.release_date || b.first_air_date)
+        ) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
+      res.status(200).json({
+        data: response.data,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = PersonController;
--- server/routes/person.js
+++ server/routes/person.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const Person = require("../controllers/personController");
 
 router.get("/popular", Person.popular);
+router.get("/:personId/combined_credits", Person.detailCredits);
+router.get("/:personId", Person.detail);
 
 module.exports = router;
